80a03dd2e9d147c7357b04f2efb046abfa4d10f0
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-01-18 16:44:47 -0300

    Aprendendo sobre condicionais.

---
+++ CursoJS/Aula11/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 60.5
+
+console.log(`Á valocidade do seu carro é ${velocidade}Km/h`)
+
+if (velocidade < 60) {
+    console.log(`Dirija sempre usando cinto de segurança`)
+} else {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
